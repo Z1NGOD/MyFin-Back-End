ea7804df4228ef0e0820b7261eca06549a7f0a1d
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-04-20 23:49:23 +0200

    build(MF-39): configured eslint, prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+const config = require('@vercel/style-guide/prettier');
+
+module.exports = {
+  ...config,
+  trailingComma: 'all',
+  singleQuote: true,
+};
--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,8 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, type TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
+describe('appController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
