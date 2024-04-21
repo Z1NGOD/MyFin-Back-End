15b87ded28af265546e08e382ec9b864943fc57b
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-04-21 22:58:10 +0200

    feat(MF-46): configured db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './libs/db/db.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
