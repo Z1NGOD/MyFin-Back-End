54bb61c1d522cb6ecce8686cae6ec54c70da3298
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-04-21 16:26:50 +0200

    feat(MF-49): configured helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Security
+  app.use(helmet());
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
