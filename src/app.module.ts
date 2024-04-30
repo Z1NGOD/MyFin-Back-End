9c247486b4e72047e3810950efefa71f8c89fc9e
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-04-30 19:44:46 +0200

    fix(MF-68): fixed wrong route for redis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { redisConfig } from '@libs/db/configs/redis.config';
+import { redisConfig } from './libs/redis/configs/redis.config';
 import { DbModule } from './libs/db/db.module';
 import { AuthModule } from './core/auth/auth.module';
 import { RedisModule } from './libs/redis/redis.module';
