4607cb7a32d8ae180e767294e59ba5d0eef8a2af
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-05-08 14:41:27 +0200

    chore(MF-48): refactored the jwt registrer to async

--- src/libs/security/security.module.ts
+++ src/libs/security/security.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { TokenService } from './services';
 import { AccessTokenStrategy } from './strategies';
 import { RefreshTokenStrategy } from './strategies/refresh-token.strategy';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret:
-        '1d82cbbdec9ad9069f0e18b293bee4f1bbaf8749b587f495022ccfbe8175824f',
-      signOptions: { expiresIn: 60 },
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string>('JWT_EXPIRE_TIME') },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [TokenService, AccessTokenStrategy, RefreshTokenStrategy],
