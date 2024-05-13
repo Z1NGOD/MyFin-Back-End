2f7b5527d982c9079c39b2494c7e949de399f473
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-05-13 21:39:16 +0200

    chore(MF-96): configured schemas

---
+++ src/libs/db/models/currency.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CurrenciesDocument = HydratedDocument<Currency>;
+
+@Schema()
+export class Currency {
+  @Prop()
+  details: string;
+  @Prop()
+  symbol: string;
+  @Prop()
+  exchangeRate: number;
+}
+
+export const CurrencySchema = SchemaFactory.createForClass(Currency);
---
+++ src/libs/db/models/index.ts
@@ -0,0 +1,5 @@
+import { Category } from './category.schema';
+import { Currency } from './currency.schema';
+import { Expense } from './expenses.schema';
+
+export const schemas = { Category, Currency, Expense };
