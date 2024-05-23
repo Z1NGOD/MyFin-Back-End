56cadf2091d6cae0af0539b61afac79a51d33dbf
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-05-23 16:09:32 +0200

    fix(MF-97): fixed wrong exchange rates

--- src/libs/db/data/migration.ts
+++ src/libs/db/data/migration.ts
@@ -9,18 +9,18 @@ export const migrationDataJson = [
     $oid: '664f21cb9732f96eb46abe7a',
     name: 'EUR',
     symbol: '€',
-    exchangeRate: 1,
+    exchangeRate: 0.92,
   },
   {
     $oid: '664f21cb9732f96eb46abe7b',
     name: 'UAH',
     symbol: '₴',
-    exchangeRate: 1,
+    exchangeRate: 40,
   },
   {
     $oid: '664f21cb9732f96eb46abe7c',
     name: 'GBP',
     symbol: '£',
-    exchangeRate: 1,
+    exchangeRate: 0.79,
   },
 ];
