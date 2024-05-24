6750229e17176a668ef4f45dcb15868f369b2e10
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-05-24 22:08:30 +0200

    test: fixed the e2e again

--- src/libs/db/migrations/currency.migration.ts
+++ src/libs/db/migrations/currency.migration.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Currency } from '@libs/db/models/currency.schema';
+import { Currency } from '../models/currency.schema';
 import { migrationDataJson } from '../data/currency.migration.data';
 
 @Injectable()
