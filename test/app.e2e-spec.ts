1f91996ad645b9a22fd44162e3e9f2bf38dab3cd
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-06-05 16:24:00 +0200

    fix: trying to fix bad res

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -171,7 +171,7 @@ describe('appController (e2e)', () => {
             refreshToken: loginResponce.refreshToken,
           })
           .expect(201);
-        expect(res.body).toBeTruthy();
+        expect(res).toHaveProperty('headers');
       });
 
       it('unsuccessfull', async () => {
