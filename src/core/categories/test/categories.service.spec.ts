64cf7fbd1423cb9ac6a739f38057838258a4ebf9
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-08-11 17:15:01 +0200

    test: initial tests for the currencies and expenses

--- src/core/categories/test/categories.service.spec.ts
+++ src/core/categories/test/categories.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, type TestingModule } from '@nestjs/testing';
+import { CategoriesRepository } from '@libs/db/repositories/categories.repository';
 import { CategoriesService } from '../services/categories.service';
 
 describe('categoriesService', () => {
@@ -6,7 +7,15 @@ describe('categoriesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        {
+          provide: CategoriesRepository,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
@@ -14,5 +23,20 @@ describe('categoriesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of categories', async () => {
+      const result = [
+        {
+          _id: 'category-id',
+          name: 'category-name',
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result as any);
+
+      expect(await service.findAll()).toBe(result);
+      expect(service.findAll).toHaveBeenCalled();
+    });
   });
 });
--- src/core/currencies/test/currencies.service.spec.ts
+++ src/core/currencies/test/currencies.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, type TestingModule } from '@nestjs/testing';
+import { CurrenciesRepository } from '@libs/db/repositories/currencies.repository';
 import { CurrenciesService } from '../services/currencies.service';
 
 describe('currenciesService', () => {
@@ -6,7 +7,15 @@ describe('currenciesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService],
+      providers: [
+        CurrenciesService,
+        {
+          provide: CurrenciesRepository,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<CurrenciesService>(CurrenciesService);
@@ -14,5 +23,22 @@ describe('currenciesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of currencies', async () => {
+      const result = [
+        {
+          _id: 'currency-id',
+          name: 'name',
+          symbol: 'symbol',
+        },
+      ];
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(result as any);
+
+      expect(await service.findAll()).toBe(result);
+      expect(service.findAll).toHaveBeenCalled();
+    });
   });
 });
